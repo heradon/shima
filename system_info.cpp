b14ee64e60b6c6f8a2bb85e09869dfdc80dbc9a6
heradon <webmaster@rmatthes.de>
2015-04-28 13:05:32 +0200

    testing code

--- system_info.cpp
+++ system_info.cpp
@@ -1,5 +1,7 @@
 #include "system_info.h"
 #include "ui_system_info.h"
+//#include "sys/types.h"
+//#include "sys/sysinfo.h"
 
 SystemInfo::SystemInfo(QWidget *parent) :
     QDialog(parent),
@@ -12,3 +14,13 @@ SystemInfo::~SystemInfo()
 {
     delete ui;
 }
+
+/* struct sysinfo memInfo;
+
+sysinfo (&memInfo);
+long long totalVirtualMem = memInfo.totalram;
+
+totalVirtualMem += memInfo.totalswap;
+totalVirtualMem *= memInfo.mem_unit;
+*/
+// http://stackoverflow.com/questions/63166/how-to-determine-cpu-and-memory-consumption-from-inside-a-process
